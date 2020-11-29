62f97aa70a451a1f4310e0c3fb2efdd11669aa85
kizumo25 <k.patagonic5@gmail.com>
2020-11-29 23:04:26 +0900

    update show new

--- task_post_app/app/controllers/posts_controller.rb
+++ task_post_app/app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def show
+		@post = Post.find_by(id: params[:id])
+	end
+
+	def new
+	end
+
+	def create
+	end
+
 end
