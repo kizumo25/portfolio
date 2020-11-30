5e96fed9006a4eb388dea5e37f4e63ecc5b89f13
kizumo25 <k.patagonic5@gmail.com>
2020-11-30 12:05:27 +0900

    update new create

--- task_post_app/app/controllers/posts_controller.rb
+++ task_post_app/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all
+		@posts = Post.all.order(created_at: :desc)
 	end
 
 	def show
@@ -11,6 +11,9 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.new(content: params[:content])
+		@post.save
+		redirect_to("/posts/index")
 	end
 
 end
